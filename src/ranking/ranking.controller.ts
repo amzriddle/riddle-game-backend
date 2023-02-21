e5f6185423f263a537d5c7ab798d354a9927b031
darth-ahsoka <79472350+amzriddle@users.noreply.github.com>
2023-02-21 02:34:28 +0000

    feat: add pagination to ranking route

--- src/ranking/ranking.controller.ts
+++ src/ranking/ranking.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Query } from "@nestjs/common";
 import { RankingService } from "./ranking.service";
+import { RankingPageDto } from "./dto/ranking-page.dto";
 
 @Controller("ranking")
 export class RankingController {
   constructor(private readonly rankingService: RankingService) {}
 
   @Get("")
-  findRanking() {
-    return this.rankingService.findRanking();
+  findRanking(@Query() query: RankingPageDto) {
+    return this.rankingService.findRanking(query);
   }
 }
--- src/ranking/ranking.service.ts
+++ src/ranking/ranking.service.ts
@@ -4,15 +4,19 @@ import { Exclude } from "class-transformer";
 import { PrismaService } from "src/prisma/prisma.service";
 
 function toJson(data) {
-    return JSON.stringify(data, (_, v) => typeof v === 'bigint' ? `${v}n` : v)
-        .replace(/"(-?\d+)n"/g, (_, a) => a);
+  return JSON.stringify(data, (_, v) =>
+    typeof v === "bigint" ? `${v}n` : v
+  ).replace(/"(-?\d+)n"/g, (_, a) => a);
 }
 
 @Injectable()
 export class RankingService {
   constructor(private prisma: PrismaService, private config: ConfigService) {}
 
-  async findRanking() {
+  async findRanking(query) {
+    const limit = 5;
+    const offset = (query.page - 1) * limit;
+
     const ranking = await this.prisma.$queryRaw`
       SELECT 
         users.email, 
@@ -22,8 +26,10 @@ export class RankingService {
       RIGHT JOIN "ChallengeComplete" 
       ON "users"."id" = "ChallengeComplete"."userId"
       GROUP BY users.id
+      LIMIT ${limit}
+      OFFSET ${offset}
     `;
-    
-    return toJson(ranking)
+
+    return toJson(ranking);
   }
 }
